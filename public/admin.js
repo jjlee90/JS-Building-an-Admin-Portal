8ea10ebf435700ef0df1e8add1276adb344fa395
jjlee90 <jlee190@hotmail.com>
2022-04-23 17:18:56 -0400

    added event listener to button to update quantity

--- public/admin.js
+++ public/admin.js
@@ -1,14 +1,3 @@
-// Your Code Here
-// fetch('http://127.0.0.1:3000/', {
-//     method: 'PATCH',
-//     headers: {
-//         'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({
-
-//     })
-// });
-
 // created a function to change book title
 async function changeBookName() {
     let res = await fetch('http://127.0.0.1:3001/updateBook', {
@@ -17,38 +6,48 @@ async function changeBookName() {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            id: 3,
-            title: 'The Legends of Arathrae',
-
-
+            id: 1,
+            quantity: 4
         })
     })
     let txt = await res.json()
     console.log(txt)
 }
+changeBookName()
 
-// appending book titles,input and button to page
+// appending book titles, input and button to page
 async function getBooks() {
-    let res = await fetch('http://127.0.0.1:3001/listBooks')
+    let res = await fetch('http://localhost:3001/listBooks')
     let txt = await res.json()
     console.log(txt)
 
-
     for (let i = 0; i < txt.length; i++) {
-        let div = document.createElement('div')
-        div.innerHTML = txt[i].title
-        document.body.append(div)
+
+        let list = document.createElement('li')
+        list.innerHTML = txt[i].title
+        document.body.append(list)
 
         let input = document.createElement('input')
-        input.value = "book quantity"
-        div.append(input)
+        input.value = txt[i].quantity
+        list.append(input)
 
         let button = document.createElement('button')
         button.innerHTML = "Save"
-        div.append(button)
+        list.append(button)
+
+        // attaching event listener to button to update quantity based on the input passed in 
+        button.addEventListener('click', () => {
+            fetch('http://localhost:3001/updateBook', {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: txt[i].id,
+                    quantity: input.value
+                })
+            })
+        })
     }
 }
-
 getBooks()
-    // document.body.append(getBooks)
-    // changeBookName()
--- server.js
+++ server.js
@@ -12,23 +12,23 @@ async function main() {
 
     app.use(bodyParser.json());
 
-    app.get('/listBooks', async (req, res) => {
+    app.get('/listBooks', async(req, res) => {
         let books = await loadBooks()
         res.json(books);
     })
 
-    app.patch('/updateBook', async (req, res) => {
+    app.patch('/updateBook', async(req, res) => {
         let books = await loadBooks()
         if (!req.body.id) return res.status(400).json({ error: true, message: `'id' is required in the request body when calling 'updateBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.` })
         let book = books.find(book => book.id === req.body.id);
         if (!book) return res.status(404).json({ error: true, message: `Could not find a book with an id of ${req.body.id}` })
-        const { title, year, quantity, imageURL, description } = { ...book, ...req.body };
+        const { title, year, quantity, imageURL, description } = {...book, ...req.body };
         Object.assign(book, { title, year, quantity, imageURL, description });
         await saveBooks(books)
         res.json(book)
     })
 
-    app.post('/addBook', async (req, res) => {
+    app.post('/addBook', async(req, res) => {
         let books = await loadBooks()
         if (!req.body.title) return res.status(400).json({ error: true, message: `'title' is required in the request body when calling 'addBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.` })
         if (!req.body.quantity) return res.status(400).json({ error: true, message: `'quantity' is required in the request body when calling 'addBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.` })
@@ -42,7 +42,7 @@ async function main() {
         res.json(book)
     })
 
-    app.delete('/removeBook/:id', async (req, res) => {
+    app.delete('/removeBook/:id', async(req, res) => {
         let books = await loadBooks()
         if (!req.params.id) return res.status(400).json({ error: true, message: `'id' is required in the request body when calling 'updateBook'. Make sure you're stringifying the body of your request, and sending the appropriate headers.` })
         let bookToDelete = books.find(book => book.id === parseInt(req.params.id));
